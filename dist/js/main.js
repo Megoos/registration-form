7f3a467d2f14aa69ccd1fa6f1abc9be6ddc52295
Megoos <meguskov@gmail.com>
2017-11-08 20:01:08 +0300

    validation form input, add datepicker and phonemask

---
+++ dist/js/main.js
@@ -0,0 +1,12 @@
+$(function($){
+  $("#phone").mask("+7(999)999-9999");
+});
+
+$(function() {
+  $("#datepick").datepicker({
+    autoClose: true,
+    view: "years",
+    language: "my-lang",
+    maxDate: new Date()
+  });
+});
