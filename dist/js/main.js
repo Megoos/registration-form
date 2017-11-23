0155eb94ee7b8a1dca52d879cb8c178e73c2aeb7
Megoos <meguskov@gmail.com>
2017-11-23 18:30:22 +0300

    edit font size, styling select, checkbox + radio margin

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,18 @@
-$(function($){
-  $("#phone").mask("+7(999)999-9999");
+$(function($) {
+  $('#phone').mask('+7(999)999-9999');
 });
 
 $(function() {
-  $("#datepick").datepicker({
+  $('#datepick').datepicker({
     autoClose: true,
-    view: "years",
-    language: "my-lang",
+    view: 'years',
+    language: 'my-lang',
     maxDate: new Date()
   });
 });
+
+(function($) {
+  $(function() {
+    $('.select').styler();
+  });
+})(jQuery);
